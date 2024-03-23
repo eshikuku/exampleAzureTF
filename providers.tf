61c8a7c5a553f25b02e4be14020b00bda214739a
eshikuku <eochieng@gmail.com>
2024-03-22 21:33:36 -0400

    adding other files

---
+++ providers.tf
@@ -0,0 +1,7 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "bcd46f2a-bc9a-4da2-819d-834ec41de2f6"
+  tenant_id       = "e7f7a392-2daa-47e2-a19c-15bf9448f285"
+}
+
